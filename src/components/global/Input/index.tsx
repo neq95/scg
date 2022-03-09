afbe021b1141d5e9db06e30f1c3910306cf285fa
Slugged <neq.out.1995@gmail.com>
2022-03-09 07:21:30 +0300

    initial commit

---
+++ src/components/global/Input/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Input: React.FC = () => {
+  return (
+    <div>
+      <label>
+        Лэйбл
+      </label>
+
+      <input /> 
+
+      <p>
+        Текст подсказки 
+      </p>
+    </div>
+  )
+}
+
+export default Input; 
