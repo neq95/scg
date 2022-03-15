1bb3bb0d090d7c65e264c9666dd4c917e3b8933e
Slugged <neq.out.1995@gmail.com>
2022-03-15 08:53:40 +0300

    added main styles for buttons

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,14 +4,23 @@ import cn from 'classnames';
 import styles from './styles.module.css';
 
 interface propsInterface {
-  variant?: 'contained' | 'outlined' | 'text';
+  variant?: 'contained' | 'text';
   startIcon?: React.FC;
   endIcon?: React.FC;
+  size?: 'small' | 'medium' | 'big' | 'large';
+  disabled?: boolean;
 }
 
-const Button : React.FC<propsInterface> = ({variant = 'text', startIcon, endIcon, children}) => {
+const Button : React.FC<propsInterface> = ({
+  variant = 'text',
+  startIcon,
+  endIcon,
+  size = 'small',
+  disabled,
+  children
+}) => {
   return (
-    <button className={cn(styles.button, styles[variant])}>
+    <button className={cn(styles.button, styles[variant], styles[size])} disabled={disabled}>
       {children}
     </button>
   )
--- src/features/Gui/GuiButtons/index.tsx
+++ src/features/Gui/GuiButtons/index.tsx
@@ -6,9 +6,71 @@ import Button from 'components/Button';
 const GuiButtons = () => {
   return (
     <GuiSection title="Кнопки">
-      <GuiSectionBlock title="Обычные">
-        <Button>
-          Это моя кнопка
+      <GuiSectionBlock title="компонент Button, variant text">
+        <Button size="small">
+          Small
+        </Button>
+
+        <Button size="small" disabled>
+          Small disabled
+        </Button>
+
+        <Button size="medium">
+          Medium
+        </Button>
+
+        <Button size="medium" disabled>
+          Medium disabled
+        </Button>
+
+        <Button size="big">
+          Big
+        </Button>
+
+        <Button size="big" disabled>
+          Big disabled
+        </Button>
+
+        <Button size="large">
+          Large
+        </Button>
+
+        <Button size="large" disabled>
+          Large disabled
+        </Button>
+      </GuiSectionBlock>
+
+      <GuiSectionBlock title="компонент Button, variant contained">
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+
+        <Button variant="contained" size="small" disabled>
+          Small disabled
+        </Button>
+
+        <Button variant="contained" size="medium">
+          Medium
+        </Button>
+
+        <Button variant="contained" size="medium" disabled>
+          Medium disabled
+        </Button>
+
+        <Button variant="contained" size="big">
+          Big
+        </Button>
+
+        <Button variant="contained" size="big" disabled>
+          Big disabled
+        </Button>
+
+        <Button variant="contained" size="large">
+          Large
+        </Button>
+
+        <Button variant="contained" size="large" disabled>
+          Large disabled
         </Button>
       </GuiSectionBlock>
     </GuiSection>
